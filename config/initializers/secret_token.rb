9abb99f7a55bd54edd3b00146e1c0e3fce7dba6d
vagrant <troytreynolds@gmail.com>
2015-04-23 11:49:20 -0400

    newProject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project12::Application.config.secret_token = '6c9cd85a90dbd654222e47d78f3dec6aa5916a51cccc4b37dcefc0c52fbe1079f1d9df0610167e232221bbaaa935e97f6a3e7e16f02ee9d39e51584b6c76a745'
