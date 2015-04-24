330f5889ce5d48e6d6b73b6d2dab0f48d304fa29
troytreynolds <troytreynolds@gmail.com>
2015-04-24 10:51:21 -0400

    Made nginx restart after deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,13 +39,15 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+after 'deploy:publishing','deploy:restart'
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+  task :restart  do
+    on roles(:app), in: :sequence, wait: 5 do
       # Here we can do anything such as:
-      # within release_path do
+      execute :touch, release_path.join("tmp/restart.txt")  
+ # within release_path do
       #   execute :rake, 'cache:clear'
       # end
     end
